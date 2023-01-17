c890544710e2434c54b82bcf30b4bb2dd9012970
jose og <yusuosman113@gmail.com>
2023-01-17 22:43:31 +0300

    flutter login with auth

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 130, 81, 128);
+const kPrimaryLightColor = Color.fromARGB(255, 78, 181, 116);
+
+const double defaultPadding = 16.0;
